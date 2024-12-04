e4c81078264b71863787d551c3f4b8dbca6087cc
Hasher Khan <hasherk102902@gmail.com>
2024-12-04 17:05:31 -0600

    Initial Commit

---
+++ Tasker/TaskerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskerApp.swift
+//  Tasker
+//
+//  Created by Hasher Khan on 12/4/24.
+//
+
+import SwiftUI
+
+@main
+struct TaskerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
